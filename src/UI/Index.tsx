d88b39e16646a6d69daa6a1680266e35a408ace6
DrunkTaric <comborush.init@proton.me>
2024-04-18 22:54:02 +0200

    smal typo

--- src/UI/Index.tsx
+++ src/UI/Index.tsx
@@ -1,9 +1,11 @@
 import { Screen } from "../config";
 import Chat from "./Chat";
+import { useEffect } from "react"
 
 export default function Index() {
+
     return (
-        <div id="ui-position" className={`flex absolute z-10 w-full h-full justify-center items-center`} onKeyDown={() => console.log("down")} onKeyUp={() => console.log("down")}>
+        <div id="ui-position" className={`flex absolute z-10 w-full h-full justify-center items-center`} onKeyDown={() => console.log("down")} onKeyUp={() => console.log("up")}>
             <div id="ui-wrapper" className="flex" style={{width: Screen.width, height: Screen.height}}>
                 <Chat></Chat>
             </div>
